1cff9f6e9a753557b7b2ecf61cf11fb62807a337
Salam Dawood <salamd135@gmail.com>
2017-11-14 17:15:28 -0500

    created main menu to start game

--- CSI5230Assignment4/app/src/main/java/edu/csi5230/salamdawood/csi5230assignment4/MainActivity.java
+++ CSI5230Assignment4/app/src/main/java/edu/csi5230/salamdawood/csi5230assignment4/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.csi5230.salamdawood.csi5230assignment4;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,131 +10,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView turnLabel = null;
     Button startButton = null;
-    TTTButton[] tttButton = new TTTButton[9];
-    Player[] players = new Player[2];
-    Player currentPlayer = null;
-    int currentTurn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        turnLabel = (TextView) findViewById(R.id.player_turn);
-        startButton = (Button) findViewById(R.id.start_game);
 
-        tttButton[0] = (TTTButton) findViewById(R.id.button0);
-        tttButton[1] = (TTTButton) findViewById(R.id.button1);
-        tttButton[2] = (TTTButton) findViewById(R.id.button2);
-        tttButton[3] = (TTTButton) findViewById(R.id.button3);
-        tttButton[4] = (TTTButton) findViewById(R.id.button4);
-        tttButton[5] = (TTTButton) findViewById(R.id.button5);
-        tttButton[6] = (TTTButton) findViewById(R.id.button6);
-        tttButton[7] = (TTTButton) findViewById(R.id.button7);
-        tttButton[8] = (TTTButton) findViewById(R.id.button8);
+        startButton = (Button) findViewById(R.id.startGamebutton);
 
-        init();
-
-    }
-
-    void swapPlayer () {
-
-        if (CheckForWinner()){
-            turnLabel.setText(currentPlayer.getPlayerName() + " has won the game.");
-
-            for (int i=0; i < 9 ; i++) {
-                tttButton[i].setEnabled(false);
-            }
-        }
-        else if (currentTurn > 8){turnLabel.setText("No Winner");}
-        else {
-            currentTurn++;
-            currentPlayer = currentPlayer == players[0] ? players[1] : players[0];
-
-            turnLabel.setText("Player: " + currentPlayer.getPlayerName());
-        }
-
-    }
-
-    public void init() {
-
-        currentPlayer = players[0] = new Player("Michael Jackson", "X");
-        players[1] = new Player("James Bond", "O");
-
-        turnLabel.setText("Game is not started yet!!!");
-
-        for (int i = 0; i < 9; i++)
-        {
-            String buttonId = "button" + i;
-
-            tttButton[i].setIndex(i);
-            players[0].cells[i] = new DataCell("");
-            players[0].cells[i].addObserver(tttButton[i]);
-            players[1].cells[i] = new DataCell("");
-            players[1].cells[i].addObserver(tttButton[i]);
-
-            tttButton[i].setEnabled(false);
-
-            tttButton[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    TTTButton source = (TTTButton) view;
-                    int index = source.getIndex();
-                    currentPlayer.cells[index].setValue(currentPlayer.getSymbol());
-                    swapPlayer();
-                    source.setEnabled(false);
-                }
-            });
-        }
-        startButton.setText("Start Game");
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StartGame();
+                Intent intent = new Intent(view.getContext(), PlayGame.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                view.getContext().startActivity(intent);
             }
         });
     }
 
-    public void StartGame() {
-        turnLabel.setText("Player: " + currentPlayer.getPlayerName());
 
-        for (int i=0; i<9; i++){
-            //enable buttons and clear any value from last game
-            tttButton[i].setText("");
-            tttButton[i].setEnabled(true);
-            //clear data cell values from previous game played
-            players[0].cells[i].setValue("");
-            players[1].cells[i].setValue("");
-
-            currentTurn = 1;
-
-        }
-
-    }
-
-    public boolean CheckForWinner() {
-
-        String playerSymbol = currentPlayer.getSymbol();
-
-        if (currentPlayer.cells[0].getValue().equals(playerSymbol) && currentPlayer.cells[1].getValue().equals(playerSymbol) && currentPlayer.cells[2].getValue().equals(playerSymbol))
-            return true;
-        if (currentPlayer.cells[3].getValue().equals(playerSymbol)&& currentPlayer.cells[4].getValue().equals(playerSymbol) && currentPlayer.cells[5].getValue().equals(playerSymbol))
-            return true;
-        if (currentPlayer.cells[6].getValue().equals(playerSymbol)&& currentPlayer.cells[7].getValue().equals(playerSymbol) && currentPlayer.cells[8].getValue().equals(playerSymbol))
-            return true;
-        if (currentPlayer.cells[0].getValue().equals(playerSymbol)&& currentPlayer.cells[3].getValue().equals(playerSymbol) && currentPlayer.cells[6].getValue().equals(playerSymbol))
-            return true;
-        if (currentPlayer.cells[1].getValue().equals(playerSymbol)&& currentPlayer.cells[4].getValue().equals(playerSymbol) && currentPlayer.cells[7].getValue().equals(playerSymbol))
-            return true;
-        if (currentPlayer.cells[2].getValue().equals(playerSymbol)&& currentPlayer.cells[5].getValue().equals(playerSymbol) && currentPlayer.cells[8].getValue().equals(playerSymbol))
-            return true;
-        if (currentPlayer.cells[0].getValue().equals(playerSymbol)&& currentPlayer.cells[4].getValue().equals(playerSymbol) && currentPlayer.cells[8].getValue().equals(playerSymbol))
-            return true;
-        if (currentPlayer.cells[2].getValue().equals(playerSymbol)&& currentPlayer.cells[4].getValue().equals(playerSymbol) && currentPlayer.cells[6].getValue().equals(playerSymbol))
-            return true;
-
-        return false;
-    }
 }
